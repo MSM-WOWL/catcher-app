d618986c5ee5b7b64eb99ae2aec1ca88e2403fc0
MSM-WOWL <decevollily@gmail.com>
2022-08-09 20:23:01 +0900

    validates追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, presence: true
+  validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   validates :nickname, presence: true
 
   has_many :posts
