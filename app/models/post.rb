1ad525987104546fc2be3f8d73906093455107b6
MSM-WOWL <decevollily@gmail.com>
2022-08-16 18:41:40 +0900

    validates追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  validates :store_name, presence: true
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/}
+
   belongs_to :user
   belongs_to :product
 end
